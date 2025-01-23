a51a4b6b1273d941351d920d3fb5113051bd7c6e
roguecat0 <roguecat0@gmail.com>
2025-01-23 16:49:50 +0100

    bare app

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
+use iced::{
+    widget::{button, text},
+    Element,
+};
 
+#[derive(Clone, Default, Debug)]
+struct App {
+    path: String,
+    actions: Vec<String>,
+    areas: Vec<String>,
+}
+#[derive(Clone, Debug)]
+enum Message {
+    ActionButton(String),
+    AreaInput(String),
+}
+impl App {
+    pub fn update(&mut self, message: Message) {
+        match message {
+            _ => (),
+        }
+    }
+    pub fn view(&self) -> Element<Message> {
+        button(text("hello")).into()
+    }
+}
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> iced::Result {
+    iced::run("hello", App::update, App::view)
 }
